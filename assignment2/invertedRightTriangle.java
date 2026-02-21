c2566e544a73742d069f45b1e435cff854aa4803
saee jambhale <saee.jambhale44@gmail.com>
2026-02-21 21:34:01 +0530

    completed assignment 2

---
+++ assignment2/invertedRightTriangle.java
@@ -0,0 +1,15 @@
+class Demo{
+	public static void main(String[]args){
+		int n=5;
+		for(int i=n-1;i>=0;i--){
+			for(int j=0;j<=i;j++){
+													System.out.print("* ");
+				
+				}
+		System.out.println();
+
+		}
+
+	}
+
+}
