f90872fbf7042d7dd4be667aa1e03b643e4892f6
saee jambhale <saee.jambhale44@gmail.com>
2026-04-23 23:53:34 +0530

    array que

---
+++ array/twoSum_1.java
@@ -0,0 +1,16 @@
+class Demo{
+	public static void main(String[]args){
+		int arr={2,6,8,5,11};
+		int target=14;
+		for(int i=0;i<arr.length;i++){
+			for(int j=i+1;j<arr.length;j++){
+				if(arr[i]+arr[j]==target){
+					System.out.println(i+" "+j);
+					return;
+				}
+			}
+		}
+	}
+
+
+}
